82c1eb1eab5e4167845e1617f11134bc604f3bd5
Nelly Alfimov <nalfimov@varonis.com>
2020-12-09 12:51:26 +0200

    add certificate details

--- terraform-aks-k8s/providers.tf
+++ terraform-aks-k8s/providers.tf
@@ -20,11 +20,13 @@ provider "azuread" {
 }
 
 provider "kubernetes" {
-  load_config_file = false
-  insecure         = true
-  host             = azurerm_kubernetes_cluster.kubernetes_cluster.kube_config.0.host
-  username         = azurerm_kubernetes_cluster.kubernetes_cluster.kube_config.0.username
-  password         = azurerm_kubernetes_cluster.kubernetes_cluster.kube_config.0.password
+  load_config_file       = false
+  host                   = azurerm_kubernetes_cluster.kubernetes_cluster.kube_config.0.host
+  username               = azurerm_kubernetes_cluster.kubernetes_cluster.kube_config.0.username
+  password               = azurerm_kubernetes_cluster.kubernetes_cluster.kube_config.0.password
+  client_certificate     = "${base64decode(azurerm_kubernetes_cluster.kubernetes_cluster.kube_config.0.client_certificate)}"
+  client_key             = "${base64decode(azurerm_kubernetes_cluster.kubernetes_cluster.kube_config.0.client_key)}"
+  cluster_ca_certificate = "${base64decode(azurerm_kubernetes_cluster.kubernetes_cluster.kube_config.0.cluster_ca_certificate)}"
 }
 
 # Run helm release
